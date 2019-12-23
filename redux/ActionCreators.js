9e68d36bf0fcbd82c3c330263083918b4b96b4c7
senatormad <senator.mad@gmail.com>
2019-12-23 03:05:13 +0100

    Delete Favorites

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -198,3 +198,9 @@ export const postComment = (dishId, rating, author, comment) => (dispatch) => {
                 alert('Your comment could not be posted\nError: ' + error.message)});        
 
 }
+
+
+export const deleteFavorite = (dishId) => ({
+    type: ActionTypes.DELETE_FAVORITE,
+    payload: dishId
+})
